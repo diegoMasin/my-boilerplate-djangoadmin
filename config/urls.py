231fcb0507223a80f50e2892c829d1d8fd64d525
Diego Masin <diegoifce@gmail.com>
2021-03-01 16:44:34 -0300

    Ajustando rota admin.

--- config/urls.py
+++ config/urls.py
@@ -3,27 +3,29 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
-urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
-    # Django Admin, use {% url 'admin:index' %}
-    path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path(
-        "users/", include("my_boilerplate_django_admin.users.urls", namespace="users")
-    ),
-    path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+admin.site.site_header = "Boilerplate"
+admin.site.site_title = "Boilerplate Django Admin"
+admin.site.index_title = "Django Admin LTE"
+
+urlpatterns = (
+    [
+        path(settings.ADMIN_URL, admin.site.urls),
+        path("", RedirectView.as_view(url=settings.ADMIN_URL)),
+        path(
+            "users/",
+            include("my_boilerplate_django_admin.users.urls", namespace="users"),
+        ),
+        path("favicon.ico", RedirectView.as_view(url="/static/images/favicon.ico")),
+        path("robots.txt", RedirectView.as_view(url="/static/robots.txt")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
 
 
 if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
     urlpatterns += [
         path(
             "400/",
